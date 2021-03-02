11c4498605842e6d63b48c828b2dab25926dc0e4
David <David@DESKTOP-MGAJQRH>
2021-03-02 13:26:46 -0300

    mudança

---
+++ test/br/com/associacao/dao/ClienteDaoImplTest.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.associacao.dao;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import br.com.associacao.entidade.Cliente;
+
+
+/**
+ *
+ * @author David
+ */
+public class ClienteDaoImplTest {
+    
+    private Cliente cliente;
+    private ClienteDaoImpl clienteDaoImpl;
+    
+    public ClienteDaoImplTest() {
+    }
+    
+    
+    @Test
+    public void testSalvar() throws Exception {
+        System.out.println("salvar");
+    }
+}
